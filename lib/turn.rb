3889474e3d53aeb1808ebbfd4db5cf8cedf6bc36
Carrie Mathieu <carrieburks@gmail.com>
2020-08-06 23:03:28 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -44,7 +44,7 @@ def turn(board)
   index = input_to_index(x)
   if valid_move?(board, index) == TRUE
     move(board, index, value = "X")
-    display_board
+    display_board(board)
   else
     return turn(board)
   end
